90a1fa0a2d3d9fd22cc0c42f66ad3f48d328003c
Craig dunlop <craigdun9393@gmail.com>
2021-01-12 21:49:44 +0000

    passes all tests for part 1. Need to refactor code

--- src/com/craig/Main.java
+++ src/com/craig/Main.java
@@ -9,35 +9,32 @@ public class Main {
         // write your code here
         char[][] board = new char[10][10];
 
+        populateNewBoard(board);
+        printBoard(board);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the coordinates of the Aircraft carrier (5 cells)");
+        populateShipOnboard(board,scanner, 5);
+
+        System.out.println("Enter the coordinates of the Battleship (4 cells)");
+        populateShipOnboard(board,scanner, 4);
+
+        System.out.println("Enter the coordinates of the Submarine (3 cells)");
+        populateShipOnboard(board,scanner, 3);
+
+        System.out.println("Enter the coordinates of the Cruiser (3 cells)");
+        populateShipOnboard(board,scanner, 3);
+
+        System.out.println("Enter the coordinates of the Destroyer (2 cells)");
+        populateShipOnboard(board,scanner, 2);
+    }
+
+    public static void populateNewBoard(char[][] board) {
         for (int i = 0; i < 10; i++) {
             for (int j = 0; j < 10; j++) {
                 board[i][j] = '~';
             }
         }
-        printBoard(board);
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Enter the coordinates of the Aircraft carrier (5 cells)");
-        String coordinates = scanner.nextLine();
-        if (Objects.equals(coordinates.charAt(0), coordinates.charAt(3))) {
-            int row = coordinates.charAt(0) - 65;
-            int start = Character.getNumericValue(coordinates.charAt(1)) - 1;
-            int end = Character.getNumericValue(coordinates.charAt(4)) - 1;
-            for (int i = start; i <= end; i++) {
-                board[row][i] = 'O';
-            }
-        } else if (Objects.equals(coordinates.charAt(1), coordinates.charAt(4))) {
-            int column = Character.getNumericValue(coordinates.charAt(1)) - 1;
-            int start = coordinates.charAt(0) - 65;
-            int end = coordinates.charAt(3) - 65;
-            for (int i = start; i <= end; i++) {
-                board[i][column] = 'O';
-            }
-        } else {
-            System.out.println("Error cannot be placed diagonally");
-        }
-
-        printBoard(board);
     }
 
     public static void printBoard(char[][] board) {
@@ -51,4 +48,138 @@ public class Main {
             System.out.println();
         }
     }
+
+    public static boolean checkCoordinatesFreeRow(int row, int start, int end, char[][]board) {
+        boolean coordinatesFree = true;
+
+        if(start >= 1) {
+            if(board[row][start-1] == 'O'){
+                coordinatesFree = false;
+            }
+        }
+        if(end <= 8) {
+            if(board[row][end + 1] == 'O') {
+                coordinatesFree = false;
+            }
+        }
+
+        for (int i = start; i <= end; i++) {
+            if(board[row][i] == 'O') {
+                coordinatesFree = false;
+                break;
+            }
+        }
+
+        if (row > 0) {
+            for (int i = start; i <= end; i++) {
+                if(board[row - 1][i] == 'O') {
+                    coordinatesFree = false;
+                    break;
+                }
+            }
+        }
+
+        if (row < 9) {
+            for (int i = start; i <= end; i++) {
+                if(board[row + 1][i] == 'O') {
+                    coordinatesFree = false;
+                    break;
+                }
+            }
+        }
+        return coordinatesFree;
+    }
+
+    public static boolean checkCoordinatesFreeColumn(int column, int start, int end, char[][]board) {
+        boolean coordinatesFree = true;
+
+        for (int i = start; i <= end; i++) {
+            if(board[i][column] == 'O') {
+                coordinatesFree = false;
+                break;
+            }
+        }
+        if(start >= 1) {
+            if(board[start-1][column] == 'O'){
+                coordinatesFree = false;
+            }
+        }
+        if(end <= 8) {
+            if(board[end + 1][column] == 'O') {
+                coordinatesFree = false;
+            }
+        }
+
+        for (int i = start; i <= end; i++) {
+            if(board[i][column] == 'O') {
+                coordinatesFree = false;
+                break;
+            }
+        }
+
+        if (column > 0) {
+            for (int i = start; i <= end; i++) {
+                if(board[i][column - 1] == 'O') {
+                    coordinatesFree = false;
+                    break;
+                }
+            }
+        }
+
+        if (column < 9) {
+            for (int i = start; i <= end; i++) {
+                if(board[i][column + 1] == 'O') {
+                    coordinatesFree = false;
+                    break;
+                }
+            }
+        }
+        return coordinatesFree;
+    }
+
+
+    public static void populateShipOnboard(char[][] board, Scanner scanner, int lengthOfShip) {
+        boolean coordinatesOkay = false;
+        while(!coordinatesOkay) {
+            String startCoordinates = scanner.next();
+            String endCoordinates = scanner.next();
+
+            if (Objects.equals(startCoordinates.charAt(0), endCoordinates.charAt(0))) {
+                int row = startCoordinates.charAt(0) - 65;
+                int start = Math.min(Integer.parseInt(endCoordinates.substring(1)) - 1, Integer.parseInt(startCoordinates.substring(1)) - 1);
+                int end = Math.max(Integer.parseInt(endCoordinates.substring(1)) - 1, Integer.parseInt(startCoordinates.substring(1)) - 1);
+                if (end - start + 1 != lengthOfShip) {
+                    System.out.println("Error - Please enter co ordinates with the correct length- correct length should be " + lengthOfShip);
+                } else {
+                    if (checkCoordinatesFreeRow(row, start, end, board)) {
+                        for (int i = start; i <= end; i++) {
+                            board[row][i] = 'O';
+                        }
+                        coordinatesOkay = true;
+                    } else {
+                        System.out.println("Error - These coordinates overlap an existing ship, please choose different coordinates");
+                    }
+                }
+            } else if (Objects.equals(startCoordinates.substring(1), endCoordinates.substring(1))) {
+                int column = Integer.parseInt(startCoordinates.substring(1)) - 1;
+                int start = Math.min(startCoordinates.charAt(0) - 65, endCoordinates.charAt(0) - 65);
+                int end = Math.max(startCoordinates.charAt(0) - 65, endCoordinates.charAt(0) - 65);
+                if (end - start + 1 != lengthOfShip) {
+                    System.out.println("Error - Please enter co ordinates with the correct length- correct length should be " + lengthOfShip);
+                }else {
+                    if (checkCoordinatesFreeColumn(column, start, end, board)) {
+                        for (int i = start; i <= end; i++) {
+                            board[i][column] = 'O';
+                        }
+                        coordinatesOkay = true;
+                    } else {
+                        System.out.println("Error - These coordinates overlap an existing ship, please choose different coordinates");
+                    }
+                }
+            } else {
+                System.out.println("Error cannot be placed diagonally");
+            }
+        }
+        printBoard(board);
+        }
 }
